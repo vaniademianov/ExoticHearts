f4c570afb6c66f88246c49c9c85cccdfc53f8a85
vaniademianov <vanushademvit@gmail.com>
2023-02-24 13:20:56 +0200

    blocked first inventory line

--- src/main/java/net/remote/access/exohearts/Events.java
+++ src/main/java/net/remote/access/exohearts/Events.java
@@ -1,12 +1,33 @@
 package net.remote.access.exohearts;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
 public class Events implements Listener {
     @EventHandler
-    public void OnPlayerMove(PlayerMoveEvent e) {
-        e.getPlayer().sendMessage("Ви порухалися");
-    }
+//    public void OnPlayerMove(PlayerMoveEvent e) {
+//        e.getPlayer().sendMessage("Ви порухалися");
+//    }
+    public void OnPLayerJoin(PlayerJoinEvent e) {
+        ItemStack dye = new ItemStack(Material.LIGHT_GRAY_DYE);
+        ItemMeta meta = dye.getItemMeta();
+        meta.setDisplayName("§f§4§lТут може бути ваше сердечко <3");
+        dye.setItemMeta(meta);
+        for (int i = 9; i<18; i++) {
+            e.getPlayer().getInventory().setItem(i, dye);
+
+        }
+    };
+    @EventHandler
+    public void OnInventoryClick(InventoryClickEvent e) {
+        if (e.getSlot() < 18 && e.getSlot() > 8) {
+            e.setCancelled(true);
+        }
+    };
 }
--- src/main/java/net/remote/access/exohearts/ExoHearts.java
+++ src/main/java/net/remote/access/exohearts/ExoHearts.java
@@ -9,7 +9,38 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.logging.Logger;
-
+//Лазуритове Серце
+//        В 1.5 Більше досвіду
+//        Удача 1 Нескінченна
+//        64 блоків лазуриту 32 книжок серце
+//        Редстоун Серце
+//        В 2 рази більше редстоуну
+//        Подвоїний крафт для всіх редстоун компонентів типу компораторів
+//        32 Блоків редстоуну 16 повторювачів серце
+//        Залізне
+//        Пасивні големи
+//        Ремонт інструментів (злиток заліза 30 міцності)
+//        >8 блоків заліза 2 Мака серце
+//        Золоте серце
+//        Пасивні пігліни
+//        Швидкість копання 1.5
+//        16 блоків золота 2 золотих меча серце
+//        Діамантове Серце
+//        В 1.5 більше Діамантів
+//        в 1.5 більше захисту
+//        16 блоків діамантів 2 діамантових нагрудника серце
+//        Ендер Серце
+//        Захист від ендер менів
+//        Телепортує як хорус коли 1 хп
+//        16 перлів 16 очей енду нагрудника серце
+//        Шалкер Серце
+//        Урон від шалкерів 1/2
+//        x2 луту з шалкерів
+//        6 шалкер блоків 16 стержню енду серце
+//        Візер Серце
+//        Імунітет до візеру
+//        шанс 1/6 накласти ефект візерів на противника
+//        6 зірок незеру  32 Кістки
 public final class ExoHearts extends JavaPlugin {
 
     @Override
@@ -20,7 +51,7 @@ public final class ExoHearts extends JavaPlugin {
         getCommand("vers").setExecutor(new CommandExecutor() {
             @Override
             public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-                sender.sendMessage("Версія плагіну: 0.1.1 DEV BUILD");
+                sender.sendMessage("Версія плагіну: 0.0.3 DEV BUILD");
                 return true;
             }
         });
