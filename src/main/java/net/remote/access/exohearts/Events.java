c2e8722fb898b8df80d84dcaa0b2cf5c43c11fa7
vaniademianov <vanushademvit@gmail.com>
2023-02-25 20:13:31 +0200

    textures

--- src/main/java/net/remote/access/exohearts/Events.java
+++ src/main/java/net/remote/access/exohearts/Events.java
@@ -18,6 +18,7 @@ public class Events implements Listener {
         ItemStack dye = new ItemStack(Material.LIGHT_GRAY_DYE);
         ItemMeta meta = dye.getItemMeta();
         meta.setDisplayName("§f§4§lТут може бути ваше сердечко <3");
+        meta.setCustomModelData(1717);
         dye.setItemMeta(meta);
         for (int i = 9; i<18; i++) {
             e.getPlayer().getInventory().setItem(i, dye);
